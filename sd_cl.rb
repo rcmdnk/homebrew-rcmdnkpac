2b6d08460f806d2b84cab88f4cc662e1e7edfe7d
rcmdnk <rcmdnk@gmail.com>
2013-12-27 13:43:06 +0100

    added sd_cl

---
+++ sd_cl.rb
@@ -0,0 +1,13 @@
+require 'formula'
+
+SD_CL_VERSION = '0.0.1'
+class Sd_Cl < Formula
+  homepage 'https://github.com/rcmdnk/sd_cl/'
+  url 'https://github.com/rcmdnk/sd_cl.git', :tag => "v#{SD_CL_VERSION}"
+  version SD_CL_VERSION
+  head 'https://github.com/rcmdnk/sd_cl.git', :branch => 'master'
+
+  def install
+    prefix.install 'etc'
+  end
+end
