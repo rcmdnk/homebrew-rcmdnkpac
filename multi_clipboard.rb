ce868dbff1d9c5f3eea181c106925e3cfc8794e7
rcmdnk <rcmdnk@gmail.com>
2014-01-03 18:44:24 +0100

    updated

--- multi_clipboard.rb
+++ multi_clipboard.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-MULTI_CLIPBOARD_VERSION = '0.0.2'
+MULTI_CLIPBOARD_VERSION = '0.0.3'
 class MultiClipboard < Formula
   homepage 'https://github.com/rcmdnk/multi_clipboard/'
   url 'https://github.com/rcmdnk/multi_clipboard.git', :tag => "v#{MULTI_CLIPBOARD_VERSION}"
