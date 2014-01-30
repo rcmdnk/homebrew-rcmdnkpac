7e3d15329d7aa8c625bb8c159a77c1b8217ab4eb
rcmdnk <rcmdnk@gmail.com>
2014-01-30 21:53:20 +0100

    updated sd_cl

--- sd_cl.rb
+++ sd_cl.rb
@@ -1,10 +1,18 @@
 require 'formula'
 
-SD_CL_VERSION = '0.0.3'
+SD_CL_VERSION = '0.0.4'
 class SdCl < Formula
+  option 'standalone', 'stand-alone version'
+
   homepage 'https://github.com/rcmdnk/sd_cl/'
-  url 'https://github.com/rcmdnk/sd_cl.git', :tag => "v#{SD_CL_VERSION}"
-  version SD_CL_VERSION
+  if build.include 'standalone'
+    url 'https://github.com/rcmdnk/sd_cl.git', :branch => 'standalone'
+    version 'Standalone'
+    depends_on 'rcmdnk/sentaku'
+  else
+    url 'https://github.com/rcmdnk/sd_cl.git', :tag => "v#{SD_CL_VERSION}"
+    version SD_CL_VERSION
+  end
   head 'https://github.com/rcmdnk/sd_cl.git', :branch => 'master'
 
   def install
