b5ca2fe0af6bd74d5d953f7e7a6e3cddaf241910
rcmdnk <rcmdnk@gmail.com>
2014-07-21 08:41:38 +0900

    updated asciinema

--- asciinema.rb
+++ asciinema.rb
@@ -1,9 +1,10 @@
 require "formula"
 
-ASCIINEMA_VERSION = "0.9.5"
+ASCIINEMA_VERSION = "0.9.8"
 class Asciinema < Formula
   homepage "http://asciinema.org"
   url "https://github.com/sickill/asciinema/archive/v#{ASCIINEMA_VERSION}.tar.gz"
+  sha1 "d7d660f80e04754073262d955cb9ce9d8e850326"
 
   depends_on :python => "2.7"
 
