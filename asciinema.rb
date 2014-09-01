299ce0009fa5afa0d57081f4c4eb352e88fd7257
rcmdnk <rcmdnk@gmail.com>
2014-09-01 13:27:13 +0900

    updated asciinema...

--- asciinema.rb
+++ asciinema.rb
@@ -4,7 +4,7 @@ ASCIINEMA_VERSION = "0.9.8"
 class Asciinema < Formula
   homepage "http://asciinema.org"
   url "https://github.com/sickill/asciinema/archive/v#{ASCIINEMA_VERSION}.tar.gz"
-  sha1 "d7d660f80e04754073262d955cb9ce9d8e850326"
+  sha1 "d374ffebe81cc43bf9d07380836302981d249b73"
 
   depends_on :python
 
