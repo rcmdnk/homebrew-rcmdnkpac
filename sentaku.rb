78baad5e68f18dc00663ee3c9832a1cfb6bd8276
rcmdnk <rcmdnk@gmail.com>
2014-11-25 19:59:27 +0900

    updated sentaku

--- sentaku.rb
+++ sentaku.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-SENTAKU_VERSION = '0.2.5'
+SENTAKU_VERSION = '0.2.6'
 class Sentaku < Formula
   homepage 'https://github.com/rcmdnk/sentaku/'
   url 'https://github.com/rcmdnk/sentaku.git', :tag => "v#{SENTAKU_VERSION}"
