082c8c112475132efffbea00468627367e41a23f
rcmdnk <rcmdnk@gmail.com>
2015-02-02 20:46:14 +0900

    updated trash

--- rcmdnk-trash.rb
+++ rcmdnk-trash.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-RCMDNK_TRASH_VERSION = '0.1.2'
+RCMDNK_TRASH_VERSION = '0.1.3'
 class RcmdnkTrash < Formula
   homepage 'https://github.com/rcmdnk/trash/'
   url 'https://github.com/rcmdnk/trash.git', :tag => "v#{RCMDNK_TRASH_VERSION}"
