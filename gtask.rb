53de295765b049b987faf7af5083dbd39aa00c77
rcmdnk <rcmdnk@gmail.com>
2015-11-12 10:43:32 +0900

    updated gtask

--- gtask.rb
+++ gtask.rb
@@ -1,6 +1,6 @@
 class Gtask < Formula
   homepage "https://github.com/rcmdnk/gtask/"
-  url "https://github.com/rcmdnk/homebrew-file/archive/v0.0.2.tar.gz"
+  url "https://github.com/rcmdnk/gtask/archive/v0.0.2.tar.gz"
   sha256 "e923eca26ab6fa5ca9d0700f25146e725c51a3d75e1a676c2be43b3a3bc9546c"
 
   head "https://github.com/rcmdnk/gtask.git"
