8aa7abf7c4e194830f4b80741bf167b4595e899b
rcmdnk <rcmdnk@gmail.com>
2016-01-04 14:49:34 +0900

    updated rcmdnk-trash

--- rcmdnk-trash.rb
+++ rcmdnk-trash.rb
@@ -1,7 +1,7 @@
 class RcmdnkTrash < Formula
   homepage "https://github.com/rcmdnk/trash/"
-  url "https://github.com/rcmdnk/trash/archive/v0.1.6.tar.gz"
-  sha256 "ab45a57db94e386f505438f233eaf21f243644b7e03c87b8345561e878326250"
+  url "https://github.com/rcmdnk/trash/archive/v0.2.0.tar.gz"
+  sha256 "4c4f2d033bebce4a02e1987f4a095ab5856454866af29017924cbf8ecf84c99f"
   head "https://github.com/rcmdnk/trash.git"
 
   depends_on "sentaku"
