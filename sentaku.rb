4328ac7120f72a813d4f07c597a89251f05bdaa3
rcmdnk <rcmdnk@gmail.com>
2016-04-17 07:08:22 +0900

    updated sentaku

--- sentaku.rb
+++ sentaku.rb
@@ -1,8 +1,8 @@
 class Sentaku < Formula
   desc "Utility to make sentaku (selection) window with shell command."
   homepage "https://github.com/rcmdnk/sentaku/"
-  url "https://github.com/rcmdnk/sentaku/archive/v0.4.6.tar.gz"
-  sha256 "2c9801067ab7e4dd61af5ffb40b5098bc127d2093214c0fd4358c7a2d248ab2e"
+  url "https://github.com/rcmdnk/sentaku/archive/v0.4.7.tar.gz"
+  sha256 "9d300afdadb520bcfc9dff8136bb3faf995ebd26980fef873ae4f9e2d3ea625c"
 
   head "https://github.com/rcmdnk/sentaku.git"
 
