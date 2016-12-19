d7f96584c81bf9b1c6978885ebc4734aa1e71648
rcmdnk <rcmdnk@gmail.com>
2016-12-19 16:38:39 +0900

    updated shell-explorer

--- shell-explorer.rb
+++ shell-explorer.rb
@@ -1,8 +1,8 @@
 class ShellExplorer < Formula
   desc "File explorer with preview, made with shell script."
   homepage "https://github.com/rcmdnk/shell-explorer/"
-  url "https://github.com/rcmdnk/shell-explorer/archive/v0.0.2.tar.gz"
-  sha256 "38f8138c540526e67d556e5061767605877255cfaeb4a3b14d1fd9b213acb116"
+  url "https://github.com/rcmdnk/shell-explorer/archive/v0.0.3.tar.gz"
+  sha256 "6c58052eaf0273000bf381f917c7c2cef77f4fd6bd6b0e451e6b65b23cebaf46"
 
   head "https://github.com/rcmdnk/shell-explorer.git"
 
