c41e25a830b88bec618e94ef8a20c387faee0720
rcmdnk <rcmdnk@gmail.com>
2017-03-06 12:41:15 +0900

    added inputsource

--- inputsource.rb
+++ inputsource.rb
@@ -1,10 +1,9 @@
 class Inputsource < Formula
   desc "Command line tool to select text input source on Mac OS X 10.5 (Leopard) or later."
   homepage "https://github.com/hnakamur/inputsource"
-  head "https://github.com/hnakamur/inputsource"
+  head "https://github.com/hnakamur/inputsource.git"
 
   def install
     bin.install "inputsource"
-    chmod 0755, "#{bin}/inputsource"
   end
 end
