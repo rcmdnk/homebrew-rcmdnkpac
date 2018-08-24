0d2ce121a8a33b77db6df56eb1adf2e42b89ea79
rcmdnk <rcmdnk@gmail.com>
2018-08-25 08:20:00 +0900

    sd_cl updat

--- sd_cl.rb
+++ sd_cl.rb
@@ -1,8 +1,8 @@
 class SdCl < Formula
   desc "Useful functions to change directories for Bash/Zsh and GNU screen/tmux."
   homepage "https://github.com/rcmdnk/sd_cl/"
-  url "https://github.com/rcmdnk/sd_cl/archive/v1.3.0.tar.gz"
-  sha256 "96f61e707ae1c881eb58d8fad4d2b765dd0830d802dbba9e86a393710f9c2cd2"
+  url "https://github.com/rcmdnk/sd_cl/archive/v1.4.0.tar.gz"
+  sha256 "7c06a74ea07838007bd31ea40721d61188c762234cef5653868e045a5ff89a50"
 
   head "https://github.com/rcmdnk/sd_cl.git"
 
