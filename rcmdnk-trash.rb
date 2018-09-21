60090902b687142019056418f42df7c7b5ef7180
rcmdnk <rcmdnk@gmail.com>
2018-09-21 18:09:35 +0900

    updated trash

--- rcmdnk-trash.rb
+++ rcmdnk-trash.rb
@@ -1,8 +1,8 @@
 class RcmdnkTrash < Formula
   desc "Remove Command using a trash box."
   homepage "https://github.com/rcmdnk/trash/"
-  url "https://github.com/rcmdnk/trash/archive/v0.2.3.tar.gz"
-  sha256 "f911954aa526dc939970065919d098a3e6edbddf97b9b02515501f216beb7bc0"
+  url "https://github.com/rcmdnk/trash/archive/v0.2.4.tar.gz"
+  sha256 "6393eff4a9c16cc96f05ce080d0892bc58bd594d48ed1d94e2f5777972aa9c12"
 
   head "https://github.com/rcmdnk/trash.git"
 
