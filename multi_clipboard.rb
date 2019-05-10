8342900521a23d19dc3aac5535bb485f2d8b72f5
rcmdnk <rcmdnk@gmail.com>
2019-05-10 17:21:59 +0900

    update multi_clipbaord

--- multi_clipboard.rb
+++ multi_clipboard.rb
@@ -1,8 +1,8 @@
 class MultiClipboard < Formula
   desc "Clipboard manager for GNU screen."
   homepage "https://github.com/rcmdnk/multi_clipboard/"
-  url "https://github.com/rcmdnk/multi_clipboard/archive/v0.0.14.tar.gz"
-  sha256 "88c86a4c293754f8514c4e65e6727b802c0f964046ac8f8e6b48b20c8f08e93d"
+  url "https://github.com/rcmdnk/multi_clipboard/archive/v0.0.15.tar.gz"
+  sha256 "d888b193f60bc679c48095684e11ea7d7d28bc8cd559ec1fbc38900c3b7468d9"
 
   head "https://github.com/rcmdnk/multi_clipboard.git"
 
