fe5d166d3bc6149342cfe9190ab09a92a0f8b18d
rcmdnk <rcmdnk@gmail.com>
2020-04-19 20:49:33 +0900

    update parse-plist

--- parse-plist.rb
+++ parse-plist.rb
@@ -1,8 +1,8 @@
 class ParsePlist < Formula
   desc "Parser of plist in OS X."
   homepage "https://github.com/rcmdnk/parse-plist/"
-  url "https://github.com/rcmdnk/parse-plist/archive/v0.0.1.tar.gz"
-  sha256 "a52f9e470533dd7fc6b88ed771276e484c1a72e9942f83975fbe66a39f68f828"
+  url "https://github.com/rcmdnk/parse-plist/archive/v0.0.2.tar.gz"
+  sha256 "d945504ad2879a790110306fe0f464c34dd2b92c72f2323ac524a1276d5ad09d"
 
   head "https://github.com/rcmdnk/parse-plist.git"
 
