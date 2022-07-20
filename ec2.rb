829e0e5c76a89118cd311f762fde1f264cd61f84
rcmdnk <rcmdnk@gmail.com>
2022-07-20 11:45:02 +0900

    update ec2

--- ec2.rb
+++ ec2.rb
@@ -1,8 +1,8 @@
 class Ec2 < Formula
   desc "AWS CLI Wrapper for EC2"
   homepage "https://github.com/rcmdnk/ec2/"
-  url "https://github.com/rcmdnk/ec2/archive/v0.0.18.tar.gz"
-  sha256 "ad5c77f555f6db2638f60811dd43337e0fedad26c70f7f9864d2ee6de04f8380"
+  url "https://github.com/rcmdnk/ec2/archive/v0.0.19.tar.gz"
+  sha256 "6eea36e3a698ba2f0cdb83dc99f0f0725aebbeb26573b8af80f75a824fbcb1cc"
 
   head "https://github.com/rcmdnk/ec2.git"
 
