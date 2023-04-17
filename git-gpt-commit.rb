fe6321ca9ec4e20a4e18a529be88ce9abdfc197c
rcmdnk <rcmdnk@gmail.com>
2023-04-17 18:43:15 +0000

    feat: Update git-gpt-commit to v0.0.2 version.

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by ChatGPT."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.1.tar.gz"
-  sha256 "0653111af97966d496afdf2ecf73b441bbe24bf8fe0ad1079cd0eb36bb70052e"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.2.tar.gz"
+  sha256 "57e15e1744ec5a8a195957752dcc0fa9d2ad62794ae0d458917cd0e8f5f94164"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
