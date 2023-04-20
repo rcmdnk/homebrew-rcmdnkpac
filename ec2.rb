29978fedd1fcb734c501cccdc9d37ddb669f6478
rcmdnk <rcmdnk@gmail.com>
2023-04-20 20:03:59 +0900

    feat: Update ec2.rb to use the latest version (v0.0.30)

--- ec2.rb
+++ ec2.rb
@@ -1,8 +1,8 @@
 class Ec2 < Formula
   desc "AWS CLI Wrapper for EC2"
   homepage "https://github.com/rcmdnk/ec2/"
-  url "https://github.com/rcmdnk/ec2/archive/v0.0.29.tar.gz"
-  sha256 "a9a4d60aa4ec38ed7061f51b904e791bb334dc6c9adbef92d306758297f0f4ba"
+  url "https://github.com/rcmdnk/ec2/archive/v0.0.30.tar.gz"
+  sha256 "2c297fc095d8d0db5d88dd7fe55792b135691f6c7949175c840668684a4a64eb"
 
   head "https://github.com/rcmdnk/ec2.git"
 
