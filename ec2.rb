e665f246bbefabc8e30372281b0524339acfe7dc
rcmdnk <rcmdnk@gmail.com>
2023-04-24 15:22:22 +0900

    fix: Update SHA256 hash in ec2.rb formula for v0.0.32.tar.gz archive.

--- ec2.rb
+++ ec2.rb
@@ -2,7 +2,7 @@ class Ec2 < Formula
   desc "AWS CLI Wrapper for EC2"
   homepage "https://github.com/rcmdnk/ec2/"
   url "https://github.com/rcmdnk/ec2/archive/v0.0.32.tar.gz"
-  sha256 "d3add1c5628f330df0cee3434521e002b80133165ea71f48d946a87a271a009a"
+  sha256 "72fb61b4d02d410124d15e83b6d8ffbb5de03440a3c3d5bbcf7507fa7a96da5a"
 
   head "https://github.com/rcmdnk/ec2.git"
 
