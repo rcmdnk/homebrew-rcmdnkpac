db87236c9e19e10015022f441146fc0ecf29d0fc
rcmdnk <rcmdnk@gmail.com>
2023-04-26 20:24:07 +0900

    fix: Updated URL and SHA256 checksum for ec2.rb formula.

--- ec2.rb
+++ ec2.rb
@@ -1,8 +1,8 @@
 class Ec2 < Formula
   desc "AWS CLI Wrapper for EC2"
   homepage "https://github.com/rcmdnk/ec2/"
-  url "https://github.com/rcmdnk/ec2/archive/v0.0.35.tar.gz"
-  sha256 "697608c968da6038ced65d40a3116abe57c5829b1d27b7608580f515c1861553"
+  url "https://github.com/rcmdnk/ec2/archive/v0.0.36.tar.gz"
+  sha256 "843066ef5a498530481d68126e18570ec8038092c0591720433ed88acfb60569"
 
   head "https://github.com/rcmdnk/ec2.git"
 
