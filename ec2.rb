8aeef9f71cba8e566e95cd158b65926296f54850
rcmdnk <rcmdnk@gmail.com>
2023-07-05 16:46:58 +0900

    chore: Update ec2.rb to version v0.0.44 with new SHA256

--- ec2.rb
+++ ec2.rb
@@ -1,8 +1,8 @@
 class Ec2 < Formula
   desc "AWS CLI Wrapper for EC2"
   homepage "https://github.com/rcmdnk/ec2/"
-  url "https://github.com/rcmdnk/ec2/archive/v0.0.43.tar.gz"
-  sha256 "5dfed3c425ebc478e37008136e8140682616afed8200e4a9b71ab1b42b800310"
+  url "https://github.com/rcmdnk/ec2/archive/v0.0.44.tar.gz"
+  sha256 "d6b6b5db6ec410a7468b81481b3526382a19823a34e2f4c265b9c1f094fc8bd5"
 
   head "https://github.com/rcmdnk/ec2.git"
 
