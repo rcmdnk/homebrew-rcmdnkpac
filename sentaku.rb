b2010d15670121e05fae31e0f0872ed0f7c7d411
rcmdnk <rcmdnk@gmail.com>
2023-10-19 16:07:13 +0900

    feat: Updated URL and sha256 for sentaku v0.8.1

--- sentaku.rb
+++ sentaku.rb
@@ -1,8 +1,8 @@
 class Sentaku < Formula
   desc "Utility to make sentaku (selection) window with shell command."
   homepage "https://github.com/rcmdnk/sentaku/"
-  url "https://github.com/rcmdnk/sentaku/archive/v0.8.0.tar.gz"
-  sha256 "f8c643992d74ce74d79cbc8b72dcfc90bcdb6553ecd4e60e4597f4080dbb4d6c"
+  url "https://github.com/rcmdnk/sentaku/archive/v0.8.1.tar.gz"
+  sha256 "7054f83a7100e19fffd5c89719f958b7951be43d2e9b94b7f53681dfbd544874"
   license "MIT"
 
   head "https://github.com/rcmdnk/sentaku.git"
