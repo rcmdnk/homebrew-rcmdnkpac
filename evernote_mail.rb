afcaab5e5866ddbe4df1f0e4279bc4059a03820f
rcmdnk <rcmdnk@gmail.com>
2023-11-01 00:41:36 +0000

    chore: update evernote_mail.rb URL and SHA256 hashes

--- evernote_mail.rb
+++ evernote_mail.rb
@@ -1,8 +1,8 @@
 class EvernoteMail < Formula
   desc "Script to send files/comments to Evernote by a mail."
   homepage "https://github.com/rcmdnk/evernote_mail/"
-  url "https://github.com/rcmdnk/evernote_mail/archive/v0.0.6.tar.gz"
-  sha256 "82311afcab3c19c8761d1213a5e11a6b83a1d70ecb98e135096bf4f01d5054c0"
+  url "https://github.com/rcmdnk/evernote_mail/archive/v0.0.7.tar.gz"
+  sha256 "83869cac38f993d7de866be79333b39fd042f67ed8593598f3a674d6b269d4e7"
   license "MIT"
 
   head "https://github.com/rcmdnk/evernote_mail.git"
