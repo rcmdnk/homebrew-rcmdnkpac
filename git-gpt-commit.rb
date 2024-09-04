6da4ecdbf092d0bf3a77cf29b8eb5851b80d3fb3
rcmdnk <rcmdnk@gmail.com>
2024-09-04 12:34:29 +0900

    chore: update URL and SHA256 for release v0.0.7

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by ChatGPT."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.6.tar.gz"
-  sha256 "2c2576516b3e8ec5d6c24939839d2be52ae1e89403060e0e39b2ecde2a9c0ee1"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.7.tar.gz"
+  sha256 "98c753c1a355d0da642ce9827228e01fde787592fdb2d6c65bcdce151e5e4f76"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
