9026ca5ffaed2cb89fcb4d21dc1c09b0a7119265
rcmdnk <rcmdnk@gmail.com>
2024-09-05 12:56:21 +0900

    chore: update ec2 formula to version 0.1.3 with new SHA256 hash

--- ec2.rb
+++ ec2.rb
@@ -1,8 +1,8 @@
 class Ec2 < Formula
   desc "AWS CLI Wrapper for EC2"
   homepage "https://github.com/rcmdnk/ec2/"
-  url "https://github.com/rcmdnk/ec2/archive/v0.1.2.tar.gz"
-  sha256 "36500f76d96d132431fe12844c6b72ab9e096badf7ca0d3ea2c700b365243744"
+  url "https://github.com/rcmdnk/ec2/archive/v0.1.3.tar.gz"
+  sha256 "5685b3b81d225b0dc8f1753cadc308090035ea64bb89ceef25b3bcd20a196230"
 
   head "https://github.com/rcmdnk/ec2.git"
 
