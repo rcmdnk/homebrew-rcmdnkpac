37f10097b428c44f88e755399480a44f3915dd45
rcmdnk <rcmdnk@gmail.com>
2024-09-28 06:43:01 +0900

    feat: update to version 0.0.8 with new sha256 checksum

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by ChatGPT."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.7.tar.gz"
-  sha256 "98c753c1a355d0da642ce9827228e01fde787592fdb2d6c65bcdce151e5e4f76"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.8.tar.gz"
+  sha256 "427bcf37ffbc00c5d9d61d61c21cfcf3edd636d36b5574ea42eb5e048f2057d6"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
