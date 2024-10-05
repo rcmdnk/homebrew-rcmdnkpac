212e52d3b716625710d1cb3b9155150fe2e44fd7
rcmdnk <rcmdnk@gmail.com>
2024-10-05 19:41:57 +0900

    feat: update shell-logger to version 0.3.0

--- shell-logger.rb
+++ shell-logger.rb
@@ -1,8 +1,8 @@
 class ShellLogger < Formula
   desc "Logger for shell script."
   homepage "https://github.com/rcmdnk/shell-logger/"
-  url "https://github.com/rcmdnk/shell-logger/archive/v0.2.1.tar.gz"
-  sha256 "434497fba2ac293f60ffed8ad3a0010a286dabcf35fbe3abe07afd9909312389"
+  url "https://github.com/rcmdnk/shell-logger/archive/v0.3.0.tar.gz"
+  sha256 "baf21bff99b68acf26cf54cbed54a34c34fa9fb953c0adef13c7105798f7638c"
   license "MIT"
 
   head "https://github.com/rcmdnk/shell-logger.git"
