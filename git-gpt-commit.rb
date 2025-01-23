237f5cf9102ad77f6757a88c4c36bc6637759257
rcmdnk <rcmdnk@gmail.com>
2025-01-23 12:26:44 +0900

    chore: update version and sha256 for git-gpt-commit to v0.0.11

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by ChatGPT."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.10.tar.gz"
-  sha256 "0ff7e1e84d865426091a3a25d0c6eeab50a291f3b41be5f6bb38a9bfeaeb5f63"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.11.tar.gz"
+  sha256 "014d5a236a2f05acc915d733a445cb7cc1153e1538fa81dc04a57b24aa6c3505"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
