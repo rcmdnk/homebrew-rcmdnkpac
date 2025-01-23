338ccae9845df9c386642f55ef3dd0c64c7f9012
rcmdnk <rcmdnk@gmail.com>
2025-01-24 04:56:55 +0900

    docs: update description and version details in git-gpt-commit.rb

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
-  desc "Git command to make a commit message by ChatGPT."
+  desc "Git command to make a commit message by LLM."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.12.tar.gz"
-  sha256 "22918cb7ba74e81a00cc68a9a9f83e10403dda74454924bc46ee8cd0196f3bd1"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.13.tar.gz"
+  sha256 "1e9864100a5e6cfcb4ae37a33a64ef70b3f2d49f1677f0be7561bb4b97d43ed5"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
