4728be9931b1aa8cdec4c19a3297b17a1d364bcc
rcmdnk <rcmdnk@gmail.com>
2025-01-23 13:53:18 +0900

    chore: update git-gpt-commit formula to version 0.0.12

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by ChatGPT."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.11.tar.gz"
-  sha256 "014d5a236a2f05acc915d733a445cb7cc1153e1538fa81dc04a57b24aa6c3505"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.12.tar.gz"
+  sha256 "22918cb7ba74e81a00cc68a9a9f83e10403dda74454924bc46ee8cd0196f3bd1"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
