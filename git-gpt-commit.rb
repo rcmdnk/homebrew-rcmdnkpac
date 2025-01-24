43057c7f260b49dbafdc11792eda6700b28ede89
rcmdnk <rcmdnk@gmail.com>
2025-01-24 11:46:10 +0900

    chore: update git-gpt-commit formula to version 0.0.14

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by LLM."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.13.tar.gz"
-  sha256 "1e9864100a5e6cfcb4ae37a33a64ef70b3f2d49f1677f0be7561bb4b97d43ed5"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.14.tar.gz"
+  sha256 "decb01ee41952fcd43cb9f538b36450df5371a1e03914898679431655414123d"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
