5f57faa3a381e9133ceada176fa1ef5d9878b2d9
rcmdnk <rcmdnk@gmail.com>
2025-01-26 19:06:33 +0900

    chore: update version and sha256 for v0.0.16 release

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by LLM."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.15.tar.gz"
-  sha256 "76dbfa56707999b83fe60b8ecb99d87c7408674a4ea49143e56f05dbead1f254"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.16.tar.gz"
+  sha256 "34b09002046dfaa8a1f2291a521e965a68a77273b2bb672f3f078692cc44c39b"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
