e843322e4486d46bd991561752d9579101af63b6
rcmdnk <rcmdnk@gmail.com>
2025-01-26 12:53:11 +0900

    chore: update git-gpt-commit to v0.0.15 with new checksum

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by LLM."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.14.tar.gz"
-  sha256 "decb01ee41952fcd43cb9f538b36450df5371a1e03914898679431655414123d"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.15.tar.gz"
+  sha256 "76dbfa56707999b83fe60b8ecb99d87c7408674a4ea49143e56f05dbead1f254"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
