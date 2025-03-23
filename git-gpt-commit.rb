96efe92b15ec535a6a6ce60717a22cb1fd843c05
rcmdnk <rcmdnk@gmail.com>
2025-03-23 11:58:07 +0900

    chore: upgrade to v0.0.17

--- git-gpt-commit.rb
+++ git-gpt-commit.rb
@@ -1,8 +1,8 @@
 class GitGptCommit < Formula
   desc "Git command to make a commit message by LLM."
   homepage "https://github.com/rcmdnk/git-gpt-commit/"
-  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.16.tar.gz"
-  sha256 "34b09002046dfaa8a1f2291a521e965a68a77273b2bb672f3f078692cc44c39b"
+  url "https://github.com/rcmdnk/git-gpt-commit/archive/v0.0.17.tar.gz"
+  sha256 "5841cd155682e94a79f9cb0ca439709f87edccb316424ab1cd9755eed7ce1195"
   license "Apache-2.0"
 
   head "https://github.com/rcmdnk/git-gpt-commit.git"
