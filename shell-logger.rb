4302a6919209a5b5acea7f7a78a474df6a731a22
rcmdnk <rcmdnk@gmail.com>
2025-06-26 19:24:33 +0900

    chore: update shell-logger to v0.3.2 with new checksum

--- shell-logger.rb
+++ shell-logger.rb
@@ -1,8 +1,8 @@
 class ShellLogger < Formula
   desc "Logger for shell script."
   homepage "https://github.com/rcmdnk/shell-logger/"
-  url "https://github.com/rcmdnk/shell-logger/archive/v0.3.1.tar.gz"
-  sha256 "f4dcb91182e4218ce57fa9ed337a4b926c3407faf7df5c3333a87a6172f4a614"
+  url "https://github.com/rcmdnk/shell-logger/archive/v0.3.2.tar.gz"
+  sha256 "81df99c8c25c7d1f40d6e50728b107c21035f6aa522e1b06e024c98cf1d41856"
   license "MIT"
 
   head "https://github.com/rcmdnk/shell-logger.git"
